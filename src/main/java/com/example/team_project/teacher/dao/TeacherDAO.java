0a119d2956f8f8aac2889e44806cc1c1d0a70a0b
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2023-11-24 14:26:49 +0900

    TeacherDAO 추

---
+++ src/main/java/com/example/team_project/teacher/dao/TeacherDAO.java
@@ -0,0 +1,35 @@
+package com.example.team_project.teacher.dao;
+
+import com.example.team_project.teacher.dto.TeacherDTO;
+import com.example.team_project.teacher.mapper.TeacherMapper;
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class TeacherDAO {
+
+  // 변재혁님 여기 아래부터 작성 시작
+
+
+  // 남원우님 여기 아래부터 작성 시작
+
+  // 회원가입(선생님)
+  public int joinTeacher(TeacherDTO dto){
+    SqlSession sqlSession = MyBatisSessionFactory.getSqlSession();
+    TeacherMapper mapper = sqlSession.getMapper(TeacherMapper.class);
+    int result = mapper.joinTeacher(dto);
+    if (result == 1) {
+      sqlSession.commit();
+      System.out.println("새로운 Teacher 저장 성공");
+    } else {
+      System.out.println("새로운 Teacher 저장 실패");
+    }
+    sqlSession.close();
+    return result;
+  }
+
+  // 차소영님 여기 아래부터 작성 시작
+
+
+  // 유지호님 여기 아래부터 작성 시작
+
+
+}
