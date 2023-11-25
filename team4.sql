056f409cb96cca74ca66167e1249fe53cd50cd0c
jaehyukpyon2 <145942491+jaehyukpyon2@users.noreply.github.com>
2023-11-25 19:17:33 +0900

    Update team4.sql

--- team4.sql
+++ team4.sql
@@ -4,21 +4,21 @@ CREATE DATABASE team4;
 USE team4;
 
 CREATE TABLE student (
-	student_idx INT PRIMARY KEY AUTO_INCREMENT,
+    student_idx INT PRIMARY KEY AUTO_INCREMENT,
     student_id VARCHAR(100) UNIQUE NOT NULL,
     student_password VARCHAR(100),
     student_name VARCHAR(50),
     student_join_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
 CREATE TABLE teacher (
-	teacher_idx INT PRIMARY KEY AUTO_INCREMENT,
+    teacher_idx INT PRIMARY KEY AUTO_INCREMENT,
     teacher_id VARCHAR(100) UNIQUE NOT NULL,
     teacher_password VARCHAR(100) NOT NULL,
     teacher_name VARCHAR(50) NOT NULL,
     teacher_join_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
 CREATE TABLE class (
-	class_idx INT PRIMARY KEY AUTO_INCREMENT,
+    class_idx INT PRIMARY KEY AUTO_INCREMENT,
     class_name VARCHAR(200),
     class_explain TEXT,
     teacher_idx INT,
@@ -31,7 +31,7 @@ CREATE TABLE class (
     FOREIGN KEY(teacher_idx) REFERENCES teacher(teacher_idx));
     
 CREATE TABLE lesson (
-	lesson_idx INT PRIMARY KEY AUTO_INCREMENT, 
+    lesson_idx INT PRIMARY KEY AUTO_INCREMENT, 
     lesson_name VARCHAR(100),
     lesson_sequence INT,
     class_idx INT,
@@ -41,7 +41,7 @@ CREATE TABLE lesson (
     FOREIGN KEY(class_idx) REFERENCES class(class_idx));
     
 CREATE TABLE student_class ( 
-	student_idx INT,
+    student_idx INT,
     class_idx INT,
     start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(student_idx, class_idx),
@@ -49,7 +49,7 @@ CREATE TABLE student_class (
     FOREIGN KEY(class_idx) REFERENCES class(class_idx));
 
 CREATE TABLE student_lesson ( 
-	student_idx INT,
+    student_idx INT,
     class_idx INT,
     lesson_idx INT,
     study_time INT,
@@ -59,18 +59,18 @@ CREATE TABLE student_lesson (
     FOREIGN KEY(lesson_idx) REFERENCES lesson(lesson_idx));
     
 CREATE TABLE board (
-	board_idx INT PRIMARY KEY AUTO_INCREMENT,
+    board_idx INT PRIMARY KEY AUTO_INCREMENT,
     class_idx INT,
     student_idx INT,
     title VARCHAR(200),
     content TEXT,
-    visitcount int default 0, # 조회수
+    visitcount INT DEFAULT 0, # 조회수
     register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(class_idx) REFERENCES class(class_idx),
     FOREIGN KEY(student_idx) REFERENCES student(student_idx));
     
 CREATE TABLE comment (
-	comment_idx INT PRIMARY KEY AUTO_INCREMENT,
+    comment_idx INT PRIMARY KEY AUTO_INCREMENT,
     board_idx INT,
     person_idx INT, # 누가(학생or선생) 댓글을 달았는지 그 사람의 pk 값
     person_type TINYINT, # if student then 0, if teacher then 1
