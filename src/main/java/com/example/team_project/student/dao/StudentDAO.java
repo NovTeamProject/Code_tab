f3121f2e36a5cec5516122a892a068fcdd30e7a1
Eumnya <2124524@naver.com>
2023-11-26 19:08:39 +0900

    StudentDAO import문 추가

--- src/main/java/com/example/team_project/student/dao/StudentDAO.java
+++ src/main/java/com/example/team_project/student/dao/StudentDAO.java
@@ -1,6 +1,10 @@
 package com.example.team_project.student.dao;
 
+import com.example.team_project.mybatis.factory.MyBatisSessionFactory;
+import com.example.team_project.student.dto.StudentDTO;
+import com.example.team_project.student.mapper.StudentMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
 
 @Slf4j
 public class StudentDAO {
