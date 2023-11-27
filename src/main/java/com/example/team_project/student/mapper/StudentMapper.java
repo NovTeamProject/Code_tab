5ca739508907b367042838bc8ddd428b31ecb51b
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2023-11-27 16:37:35 +0900

    mapper 추가

--- src/main/java/com/example/team_project/student/mapper/StudentMapper.java
+++ src/main/java/com/example/team_project/student/mapper/StudentMapper.java
@@ -1,7 +1,8 @@
 package com.example.team_project.student.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-
+import java.util.List;
+import java.util.Map;
 @Mapper
 public interface StudentMapper {
     // 변재혁님 여기 아래부터 작성 시작
