c0b21b9c838bd165ffbdb1862f7cb7e9a71aa5b7
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2023-11-27 16:37:56 +0900

    mapper 추가

--- src/main/java/com/example/team_project/teacher/mapper/TeacherMapper.java
+++ src/main/java/com/example/team_project/teacher/mapper/TeacherMapper.java
@@ -3,7 +3,7 @@ package com.example.team_project.teacher.mapper;
 import com.example.team_project.teacher.dto.TeacherDTO;
 
 import java.util.List;
-
+import java.util.Map;
 public interface TeacherMapper {
     // 변재혁님 여기 아래부터 작성 시작
     List<TeacherDTO> getAllTeacher();
