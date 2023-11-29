ed665f6cc65e0e92ffdc037184e215fb66fbaf8a
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2023-11-30 01:59:43 +0900

    feat: student 로그인,회원가입 기능구현

--- src/main/java/com/example/team_project/student/mapper/StudentMapper.java
+++ src/main/java/com/example/team_project/student/mapper/StudentMapper.java
@@ -2,8 +2,9 @@ package com.example.team_project.student.mapper;
 
 import com.example.team_project.student.dto.StudentDTO;
 import org.apache.ibatis.annotations.Mapper;
-import java.util.List;
+
 import java.util.Map;
+
 @Mapper
 public interface StudentMapper {
     // 변재혁님 여기 아래부터 작성 시작
@@ -11,9 +12,9 @@ public interface StudentMapper {
 
     // 남원우님 여기 아래부터 작성 시작
     int joinStudent (StudentDTO dto);
-    List<StudentDTO> loginStudent(Map <String, String> map);
+    int loginStudent(Map <String, String> map);
+    StudentDTO idCheck(String studentId);
 
-    
     // 차소영님 여기 아래부터 작성 시작
 
 
