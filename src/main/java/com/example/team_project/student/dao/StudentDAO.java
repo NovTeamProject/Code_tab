41973f56f833ab8cb45cb62c696232a9d9c0c15a
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2023-11-30 17:42:15 +0900

    feat: studentDAO 구조변경

--- src/main/java/com/example/team_project/student/dao/StudentDAO.java
+++ src/main/java/com/example/team_project/student/dao/StudentDAO.java
@@ -22,8 +22,8 @@ public class StudentDAO {
     StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
     int result = mapper.joinStudent(dto);
     if (result == 1) {
-      sqlSession.commit();
       System.out.println("새로운 Student 저장 성공");
+      sqlSession.commit();
     } else {
       System.out.println("새로운 Student 저장 실패");
     }
@@ -33,11 +33,10 @@ public class StudentDAO {
     // 로그인(학생)
     public boolean loginStudent(String studentId, String studentPassword) {
       Map<String, String> map = new HashMap<>();
-        System.out.println(studentId+".."+studentPassword); //여기까지옴
+
       map.put("studentId", studentId);
       map.put("studentPassword", studentPassword);
-
-      SqlSession sqlSession = MyBatisSessionFactory.getSqlSession();    //여기가 에러sql문 에러인거같음
+      SqlSession sqlSession = MyBatisSessionFactory.getSqlSession();
       StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
       int result = mapper.loginStudent(map);
        
@@ -47,7 +46,7 @@ public class StudentDAO {
         return false;
       }
     }
-
+  // 아이디 중복 체크
   public StudentDTO idCheck(String studentId) {
 
       SqlSession sqlSession = MyBatisSessionFactory.getSqlSession();
@@ -57,6 +56,14 @@ public class StudentDAO {
       return result;
 
   }
+    public StudentDTO infoStudent(String studentId) {
+      SqlSession sqlSession = MyBatisSessionFactory.getSqlSession();
+      StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+      StudentDTO result = mapper.infoStudent(studentId);
+      sqlSession.close();
+      return result;
+    }
+
 
     // 차소영님 여기 아래부터 작성 시작
 
