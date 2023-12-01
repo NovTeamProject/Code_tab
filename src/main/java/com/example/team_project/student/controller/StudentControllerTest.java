e698488a43b39f26814fa35a35c3cbaba001bea3
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2023-12-01 14:51:00 +0900

    fix : 패키지 구조 변경

--- src/main/java/com/example/team_project/student/controller/StudentControllerTest.java
+++
@@ -1,6 +0,0 @@
-package com.example.team_project.student.controller;
-
-public class StudentControllerTest {
-    // 이 파일은 테스트 용입니다.
-    // 학생과 관련된 controller는 student/controller 패키지 안에 직접 만드세요.
-}
